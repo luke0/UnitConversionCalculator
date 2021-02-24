49ce105e37350d2f20ae96bc70dea27632950a03
luke.inger@jcb-finance.co.uk <luke.inger@jcb-finance.co.uk>
2021-02-24 19:15:04 +0000

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Luke Inger on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
